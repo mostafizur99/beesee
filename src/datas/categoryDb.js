c333a0cec868d41325c668d32337787fcb79cf5b
mostafizur99 <coder.mr11@gmail.com>
2022-10-12 23:53:35 +0600

    doc: category-data updated

--- src/datas/categoryDb.js
+++ src/datas/categoryDb.js
@@ -14,24 +14,30 @@ export const categoriesData = [
         text: "Lorem ipsum dolor amet consectetur adipisicing elit dolor!",
         id: 3
     },
+
     {
-        title: "Content Writing",
+        title: "Animation & Graphics Design",
         text: "Lorem ipsum dolor amet consectetur adipisicing elit dolor!",
         id: 4
     },
     {
-        title: "Animation & Graphics Design",
+        title: "Music & Sounds",
         text: "Lorem ipsum dolor amet consectetur adipisicing elit dolor!",
         id: 5
     },
     {
-        title: "Music & Sounds",
+        title: "Graphics & Design",
         text: "Lorem ipsum dolor amet consectetur adipisicing elit dolor!",
         id: 6
     },
     {
-        title: "Graphics & Design",
+        title: "Content Writing",
         text: "Lorem ipsum dolor amet consectetur adipisicing elit dolor!",
         id: 7
+    },
+    {
+        title: "Fashion & Design",
+        text: "Lorem ipsum dolor amet consectetur adipisicing elit dolor!",
+        id: 8
     },
 ]
